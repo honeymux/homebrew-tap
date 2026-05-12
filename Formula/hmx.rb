105691e96c335c6363fa6113d0713cc4095fe89e
Aaron Campbell <aaron@monkey.org>
2026-05-12 18:47:11 -0300

    hmx: update to release 0.107.0
    https://github.com/honeymux/honeymux/releases/tag/v0.107.0
    
    Signed-off-by: Aaron Campbell <aaron@monkey.org>

--- Formula/hmx.rb
+++ Formula/hmx.rb
@@ -1,28 +1,28 @@
 class Hmx < Formula
   desc "A new UX layer for the terminal, built on tmux"
   homepage "https://github.com/honeymux/honeymux"
-  version "0.106.0"
+  version "0.107.0"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/honeymux/honeymux/releases/download/v0.106.0/hmx-darwin-arm64"
-      sha256 "22e02e8c7c50259c87ab085d2bdc6420ebec81a7bdbeaf2bec45090ca7c673ac"
+      url "https://github.com/honeymux/honeymux/releases/download/v0.107.0/hmx-darwin-arm64"
+      sha256 "f9a560051c0c90d848d0bb5811d21482ed271974d30fb2cdc7cc57a59358afa0"
     end
     on_intel do
-      url "https://github.com/honeymux/honeymux/releases/download/v0.106.0/hmx-darwin-x64"
-      sha256 "c589db3a93fa0b16794bdf7b3a8fc731a085887fb217de1d6b062b203ad0da9a"
+      url "https://github.com/honeymux/honeymux/releases/download/v0.107.0/hmx-darwin-x64"
+      sha256 "42fd9eeeb49924b3c3b3df4e73215b2d4b096a17856a2ad3f7bb7db2679fa9c0"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/honeymux/honeymux/releases/download/v0.106.0/hmx-linux-arm64"
-      sha256 "6cbb836ac03322bd84daa62718738ab4e191aa743e0eba1392f9006185ea1681"
+      url "https://github.com/honeymux/honeymux/releases/download/v0.107.0/hmx-linux-arm64"
+      sha256 "8e48fbdf3b8956f0af4dfb0fe708af0cd4b66ec57eae01a19d35d6a0ffdbe033"
     end
     on_intel do
-      url "https://github.com/honeymux/honeymux/releases/download/v0.106.0/hmx-linux-x64"
-      sha256 "a82ab73b00ba8f43c47d033cea763f948295e68684daa28fd7dcde17a4276cbd"
+      url "https://github.com/honeymux/honeymux/releases/download/v0.107.0/hmx-linux-x64"
+      sha256 "d6e0a4c8c11e810a8e4f669039be63f18ac5e80dd9a50244c5539d52f2c46511"
     end
   end
 
